ce342a3bb031170213598c9ae5633a9f227cb762
HeshmaT <zakriamotaz@gmail.com>
2025-05-10 23:36:26 +0300

    again

--- app/Http/Controllers/API/TablereserveController.php
+++ app/Http/Controllers/API/TablereserveController.php
@@ -29,6 +29,10 @@ class TablereserveController extends Controller
         public function last_reserve(Request $request){
             $userId = $request->user()->id;
             $last=TableReserve::where('user_id', $userId)->latest()->first();
+            if (!$last) {
+    return ApiHelper::sendresponse(404, 'No table reservation found for this user',null);
+}
+
             return Apihelper::sendrespone('200','The last table reserve of user here ',new TableReserveResource($last));
 
         }
